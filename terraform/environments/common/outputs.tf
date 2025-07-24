e024fcbeaee480364ef3d8dcc7d78a77cbd4f782
Nobuchika Tanaka <n.tanaka@idcf.jp>
2025-07-25 06:31:23 +0900

    idcfクラウド上にVMを構築

---
+++ terraform/environments/common/outputs.tf
@@ -0,0 +1,6 @@
+output "access_ip_address_rancher_misc_01" {
+  value = module.compute.ip_addresses["rancher-misc-01"]
+}
+output "access_ip_address_rancher_server_01" {
+  value = module.compute.ip_addresses["rancher-server-01"]
+}
